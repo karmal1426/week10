6b7f86f0faae709ed8fb87c8d7067ca15ef6794d
“karmal1426” <alogbaomotayo1426@gmail.com>
2024-11-19 09:07:35 +0100

    feat: assignment is in assignment.html file thanks!

---
+++ text.js
@@ -0,0 +1,29 @@
+//Write a function ucFirst(str) that returns the string str with the uppercased first character, for instance:
+
+function ucFirst(str) {
+    str = 'john'
+    return str[0].toUpperCase() + str.slice(1)
+    
+}
+
+
+
+// Write a function checkSpam(str) that returns true if str contains ‘viagra’ or ‘XXX’, otherwise false.
+
+function checkSpam(str) {
+    str;
+    return str == 'buy ViAgRA now' || str == 'free xxxxx' ? 'true ': 'Try again'
+}
+
+
+checkSpam = (str) => str == 'buy ViAgRA now' || str == 'free xxxxx' ? 'true ': 'Try again';
+
+arr = [1,2,3,4,3,6,3,4]
+
+arr[arr.length-1]
+
+
+
+
+
+let payment
